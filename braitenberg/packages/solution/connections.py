214c20210cb6233da7302909d91face2de1f04ff
Nikola Zupancic <nikola.z37@hotmail.com>
2024-02-20 21:22:52 -0500

    made some matrices but they suck

--- braitenberg/packages/solution/connections.py
+++ braitenberg/packages/solution/connections.py
@@ -3,20 +3,26 @@ from typing import Tuple
 import numpy as np
 
 
+
 def get_motor_left_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[100:150, 100:150] = 1
-    res[300:, 200:] = 1
-    # ---
+
+    w, h = shape[1], shape[0]
+    res[h-int(0.70*h):h, 0:int(0.15*w)] = 1.0
+    res[h-int(0.55*h):h-int(0.05*h), int(0.15*w):int(0.35*w)] = 1.0
+    res[h-int(0.70*h):h-int(0.55*h), int(0.15*w):int(0.35*w)] = 0.5
+    res[h-int(0.70*h):h-int(0.05*h), int(0.35*w):int(0.70*w)] = 1.5
+
     return res
 
 
 def get_motor_right_matrix(shape: Tuple[int, int]) -> np.ndarray:
-    # TODO: write your function instead of this one
     res = np.zeros(shape=shape, dtype="float32")
-    # these are random values
-    res[100:150, 100:300] = -1
+
+    w, h = shape[1], shape[0]
+    res[h-int(0.70*h):h, w-int(0.15*w):w] = 1.0
+    res[h-int(0.55*h):h-int(0.05*h), w-int(0.35*w):w-int(0.15*w)] = 1.0
+    res[h-int(0.70*h):h-int(0.55*h), w-int(0.35*w):w-int(0.15*w)] = 0.5
+    
     # ---
     return res
